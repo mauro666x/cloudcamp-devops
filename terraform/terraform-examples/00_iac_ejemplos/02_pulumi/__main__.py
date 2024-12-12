992f479b2054b55d22d8ba7c59b23abe7d030a75
mauro666x <mfbenavidesc@gmail.com>
2024-12-11 19:53:18 -0500

    adding new for pulumi

--- terraform/terraform-examples/00_iac_ejemplos/02_pulumi/__main__.py
+++ terraform/terraform-examples/00_iac_ejemplos/02_pulumi/__main__.py
@@ -4,7 +4,7 @@ import pulumi
 from pulumi_aws import s3
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-cloud-camp-bucket')
+bucket = s3.Bucket('my-cloud-camp-bucket-mauricio-benavides')
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
